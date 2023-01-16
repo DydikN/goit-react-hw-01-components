d0e77dca013e8ac88f7b6b9f2b9fc81468a6b9f8
DydikN <PingvinPain@gmail.com>
2023-01-16 21:21:16 +0200

    fix Profile and Statistics

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -41,7 +41,11 @@ Profile.defaultProps = {
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
-  location: PropTypes.string,
-  avatar: PropTypes.string,
-  stats: PropTypes.objectOf(PropTypes.number),
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
